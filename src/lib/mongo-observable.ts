196a60c538b6a115ad5bcb0b3ec1bff54c8277c3
Jason Dreyzehner <jason@dreyzehner.com>
2017-08-24 03:59:27 -0400

    refactor(MongoObservable): add connect method

--- src/lib/mongo-observable.ts
+++ src/lib/mongo-observable.ts
@@ -1,16 +1,36 @@
-import { Db } from "mongodb";
+import { Db, MongoClient } from "mongodb";
+import { Observable, Observer } from "rxjs";
 
 export interface MongoObservableOptions {
   db: Db;
   collection: string;
 }
 
-export class MongoObservable {
+/**
+ * An observable which emits messages added to a given MongoDB collection.
+ */
+export class MongoObservable extends Observable<any> {
   /**
-   * Returns an observable which emits messages added to the provided collection. 
+   * Create a new MongoObservable given an open MongoDB connection. 
    */
-  public static create(opts: MongoObservableOptions) {
+  public static create(opts: { db: Db; collection: string }) {
     return new MongoObservable(opts);
   }
-  private constructor(opts: MongoObservableOptions) {}
+
+  /**
+   * Create a new MongoObservable by creating a new MongoDB connection. 
+   */
+  public static connect(opts: { uri: string; collection: string }) {
+    return Observable.create((observer: Observer<any>) => {
+      MongoClient.connect(opts.uri).then(db => {
+        return new MongoObservable({
+          collection: opts.collection,
+          db
+        }).subscribe(observer);
+      });
+    });
+  }
+  private constructor(opts: { db: Db; collection: string }) {
+    super();
+  }
 }
